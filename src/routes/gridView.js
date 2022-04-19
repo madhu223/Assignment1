b5c0d1c03e2434111321aa3ffa04e249c498242a
madhu223 <madhumorigadi@gmail.com>
2022-04-20 00:08:05 +0530

    initial status grid tile

---
+++ src/routes/gridView.js
@@ -0,0 +1,11 @@
+import GridDemo from "./gridDemo";
+
+export default function GridView() {
+    return (
+      
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Grid View</h2>
+         <GridDemo></GridDemo>
+      </main>
+    );
+  }
---
+++ src/routes/tilesView.js
@@ -0,0 +1,12 @@
+import TilesDemo from "./tilesDemo";
+
+export default function TilesView() {
+    
+    return (
+      
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Tiles View</h2>
+         <TilesDemo></TilesDemo>
+      </main>
+    );
+  }
